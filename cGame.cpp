751ffc4877e37d83246cf90899eafc95e2c637f6
João Vitor Zavatin <jvzavatin@gmail.com>
2022-04-25 20:57:31 -0300

    Lógica do programa
    Implementado parte da logica do programa.

--- cGame.cpp
+++ cGame.cpp
@@ -1,7 +1,51 @@
 #include "cGame.h"
 
+wxBEGIN_EVENT_TABLE(cGame, wxFrame)
+    EVT_BUTTON(10003, Finalizar)
+    EVT_BUTTON(10004, Finalizar)
+    EVT_TEXT_ENTER(10005, checkInput)
+
+wxEND_EVENT_TABLE()
+
 cGame::cGame() : wxFrame(nullptr, wxID_ANY, "wxLetroca - Jogando", wxPoint(30, 30), wxSize(800, 600)) {
+    wxPanel* panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxPanel* panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
+
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(panel_top, 3, wxEXPAND | wxLEFT | wxTOP | wxRIGHT, 10);
+
+    wxSizer* sizer_middle = new wxBoxSizer(wxVERTICAL);
+    wxTextCtrl* control = new wxTextCtrl(this, wxID_ANY);
+    control->SetMaxLength(8);
+    sizer_middle->Add(control, 1, wxEXPAND);
+    sizer->Add(sizer_middle, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
+    
+
+
+    sizer->Add(panel_bottom, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
+
+    wxSizer* sizer_menu = new wxBoxSizer(wxHORIZONTAL);
+    btnFinalizar = new wxButton(this, 10003, "RANKING", wxPoint(500, 10), wxSize(150, 50));
+    btnFinalizar2 = new wxButton(this, 10004, "RANKING", wxPoint(500, 10), wxSize(150, 50));
+    sizer_menu->Add(btnFinalizar, 1, wxEXPAND);
+    sizer_menu->Add(btnFinalizar2, 1, wxEXPAND);
+
+    sizer->Add(sizer_menu, 1, wxEXPAND | wxALL, 10);
+
+    this->SetSizerAndFit(sizer);
 }
 
 cGame::~cGame() {
+}
+
+
+
+void cGame::Finalizar(wxCommandEvent& evt) {
+    cGame* game = new cGame();
+    Destroy();
+    game->Show();
+    evt.Skip();
 }
--- cGame.h
+++ cGame.h
@@ -1,9 +1,70 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include "wx/wx.h"
+
+using namespace std;
 
 class cGame : public wxFrame {
 public:
-	cGame();
-	~cGame();
+    string input = "NULL";
+    string level = "level_1A.txt";
+    vector<string> lines;
+    bool gameIsOver = false;
+    int numeroDeLetras;
+
+
+    void readLevelFile() {
+        // LE O ARQUIVO DA FASE E COLOCA EM UM VECTOR DE STRING
+
+        string filename("levels/");
+        filename = filename + level;
+
+        string line;
+
+        ifstream input_file(filename);
+        if (!input_file.is_open()) {
+            cerr << "Could not open the file - '" << filename << "'" << endl;
+        }
+
+        while (getline(input_file, line)) {
+            // ENQUANTO TIVER ALGO NO ARQUIVO, PUSHBACK()
+            lines.push_back(line);
+        }
+        numeroDeLetras = lines[0].length();
+        input_file.close();
+
+    };
+
+    void checkInput(wxCommandEvent& evt) {
+        // VERIFICA A PALAVRA
+        if (input != "NULL") {
+            // SE O JOGO EXISTE
+            for (int i = 1; i < lines.size(); i++) {
+                // PERCORRE AS PALAVRAS DO VECTOR
+                for (int j = 0; j < input.length(); j++) {
+                    // PERCORRE AS LETRAS DA ENTRADA 
+                    if (input[j] == lines[i][j]) {
+                        // MOSTRA A LETRA
+                        // PONTOS +1
+                    }
+                }
+            }
+        }
+    }
+
+    void Finalizar(wxCommandEvent& evt);
+   
+    cGame();
+    ~cGame();
+
+    wxButton* btnFinalizar = nullptr;
+    wxButton* btnFinalizar2 = nullptr;
+
+    wxVector<wxButton*> vecBtn;
+
+    wxDECLARE_EVENT_TABLE();
 };
 
