4376756ce1df97c977fcd8f923a61d1c55cbe774
João Vitor Zavatin <jvzavatin@gmail.com>
2022-04-27 01:23:59 -0300

    Progredido logica do Game

--- cGame.cpp
+++ cGame.cpp
@@ -1,40 +1,65 @@
 #include "cGame.h"
 
 wxBEGIN_EVENT_TABLE(cGame, wxFrame)
-    EVT_BUTTON(10003, Finalizar)
+    EVT_BUTTON(10003, Desistir)
     EVT_BUTTON(10004, Finalizar)
     EVT_TEXT_ENTER(10005, checkInput)
-
+    EVT_TIMER(wxID_ANY, OnUpdateDisplayedTime)
 wxEND_EVENT_TABLE()
 
 cGame::cGame() : wxFrame(nullptr, wxID_ANY, "wxLetroca - Jogando", wxPoint(30, 30), wxSize(800, 600)) {
-    wxPanel* panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
-    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
-
-    wxPanel* panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
-    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
 
     wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(panel_top, 3, wxEXPAND | wxLEFT | wxTOP | wxRIGHT, 10);
 
+    readLevelFile();
+
+    // LISTA DE PALAVRAS NA FASE
+    wxSizer* sizer_top = new wxBoxSizer(wxVERTICAL);
+    sizer_top->Add(result_list, 1, wxEXPAND);
+    sizer->Add(sizer_top, 4, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 20);
+
+    // CAIXA DE INPUT DE TEXTO
     wxSizer* sizer_middle = new wxBoxSizer(wxVERTICAL);
-    wxTextCtrl* control = new wxTextCtrl(this, wxID_ANY);
-    control->SetMaxLength(8);
-    sizer_middle->Add(control, 1, wxEXPAND);
-    sizer->Add(sizer_middle, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
+    inputC->SetMaxLength(numeroDeLetras);
+    sizer_middle->Add(inputC, 1, wxEXPAND);
+    sizer->Add(sizer_middle, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 20);
     
+    // TEXTO COM AS LETRAS DA FASE
+    wxSizer* sizer_botton = new wxBoxSizer(wxVERTICAL);
+    wxStaticText* botton_text = new wxStaticText(this, wxID_ANY, lines[0], wxPoint(500, 10), wxSize(150, 50), wxALIGN_CENTRE_HORIZONTAL);
+    font = botton_text->GetFont();
+    font.MakeBold().MakeLarger();
+    botton_text->SetFont(font);
+    sizer_botton->Add(botton_text, 1, wxEXPAND);
+    sizer->Add(sizer_botton, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 20);
 
-
-    sizer->Add(panel_bottom, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
-
+    // MENU INFERIOR
     wxSizer* sizer_menu = new wxBoxSizer(wxHORIZONTAL);
-    btnFinalizar = new wxButton(this, 10003, "RANKING", wxPoint(500, 10), wxSize(150, 50));
-    btnFinalizar2 = new wxButton(this, 10004, "RANKING", wxPoint(500, 10), wxSize(150, 50));
+        // BOTOES
+    btnDesistir = new wxButton(this, 10003, "Desistir", wxPoint(500, 10), wxSize(150, 50));
+    btnFinalizar = new wxButton(this, 10004, "Finalizar", wxPoint(500, 10), wxSize(150, 50));
+        // TEMPORIZADOR NAO FUNCIONA
+    font = m_timeDisplay->GetFont();
+    font.MakeBold().MakeLarger();
+    m_timeDisplay->SetFont(font);
+    m_timer->Start();
+    UpdateDisplayedTime();
+        // PONTUAÇÃO
+    wxStaticText* pontos_text = new wxStaticText(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL | wxST_NO_AUTORESIZE | wxBORDER_SIMPLE);
+    font = pontos_text->GetFont();
+    font.MakeBold().MakeLarger();
+    //pontos_text->SetLabel();
+    pontos_text->SetFont(font);
+
+
+        // ADCIONA NO SIZER_MENU
+    sizer_menu->Add(btnDesistir, 1, wxEXPAND);
+    sizer_menu->Add(m_timeDisplay, 1, wxEXPAND);
+    sizer_menu->Add(pontos_text, 1, wxEXPAND);
     sizer_menu->Add(btnFinalizar, 1, wxEXPAND);
-    sizer_menu->Add(btnFinalizar2, 1, wxEXPAND);
 
-    sizer->Add(sizer_menu, 1, wxEXPAND | wxALL, 10);
-
+    // MAIN SIZER DO GAME
+    sizer->Add(sizer_menu, 1, wxEXPAND | wxALL, 20);
     this->SetSizerAndFit(sizer);
 }
 
@@ -42,10 +67,3 @@ cGame::~cGame() {
 }
 
 
-
-void cGame::Finalizar(wxCommandEvent& evt) {
-    cGame* game = new cGame();
-    Destroy();
-    game->Show();
-    evt.Skip();
-}
--- cGame.h
+++ cGame.h
@@ -4,16 +4,28 @@
 #include <vector>
 #include <string>
 #include "wx/wx.h"
+#include <wx/datetime.h>
 
 using namespace std;
 
 class cGame : public wxFrame {
 public:
-    string input = "NULL";
+        // VARS
     string level = "level_1A.txt";
-    vector<string> lines;
+    vector<string> lines, linesCrip;
     bool gameIsOver = false;
-    int numeroDeLetras;
+    int numeroDeLetras, pontos = 0;
+        // WXWIDG
+    wxFont font;
+    wxListBox * result_list = new wxListBox(this, wxID_ANY, wxDefaultPosition, wxSize(200, 330), 0, nullptr, wxLB_SINGLE | wxLB_ALWAYS_SB | wxLB_SORT);
+    wxTextCtrl* inputC = new wxTextCtrl(this, 10005, "", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER | wxALIGN_CENTRE_HORIZONTAL);
+    wxStaticText* m_timeDisplay = new wxStaticText(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL | wxST_NO_AUTORESIZE | wxBORDER_SIMPLE);
+    //wxStopWatch* tempo = new wxStopWatch();
+        // TIMER
+    wxTimer* m_timer = new wxTimer;
+    wxDateTime* m_startTime = new wxDateTime;
+
+    wxStopWatch sw;
 
 
     void readLevelFile() {
@@ -22,16 +34,27 @@ public:
         string filename("levels/");
         filename = filename + level;
 
-        string line;
+        string line = "", lineCrip = "*           ";
 
         ifstream input_file(filename);
         if (!input_file.is_open()) {
             cerr << "Could not open the file - '" << filename << "'" << endl;
         }
-
+        int aux = 0;
         while (getline(input_file, line)) {
             // ENQUANTO TIVER ALGO NO ARQUIVO, PUSHBACK()
+
             lines.push_back(line);
+            if (aux == 1) {
+                for (int i = 0; i < line.length(); i++) {
+                    lineCrip[i] = '*';
+                }
+                result_list->AppendString(lineCrip);
+                linesCrip.push_back(lineCrip);
+            }
+
+            aux = 1;
+            line = "";
         }
         numeroDeLetras = lines[0].length();
         input_file.close();
@@ -39,32 +62,88 @@ public:
     };
 
     void checkInput(wxCommandEvent& evt) {
-        // VERIFICA A PALAVRA
-        if (input != "NULL") {
-            // SE O JOGO EXISTE
-            for (int i = 1; i < lines.size(); i++) {
-                // PERCORRE AS PALAVRAS DO VECTOR
-                for (int j = 0; j < input.length(); j++) {
-                    // PERCORRE AS LETRAS DA ENTRADA 
-                    if (input[j] == lines[i][j]) {
-                        // MOSTRA A LETRA
-                        // PONTOS +1
-                    }
-                }
+
+        for (int i = 1; i < lines.size(); i++) {
+            // PERCORRE AS PALAVRAS DO VECTOR
+            if (inputC->GetValue().compare(lines[i]) == 0) {
+                pontos++;
+                linesCrip.erase(linesCrip.begin() + i-1);
+                linesCrip.push_back(lines[i]);
+                lines.erase(lines.begin() + i);
+
+                atualizaLista();
             }
         }
     }
 
-    void Finalizar(wxCommandEvent& evt);
-   
+    void atualizaLista() {
+        // ATUALIZA O DISPLAY LISTA
+        result_list->Clear();
+
+        for (int i = 0; i < linesCrip.size(); i++) {
+            result_list->AppendString(linesCrip[i]);
+        }
+    }
+
+    void Finalizar(wxCommandEvent& evt) {
+        // É PRA FINALIZAR O JOGO E MOSTRAR OUTRA TELA
+        cGame* game = new cGame();
+        Destroy();
+        game->Show();
+        evt.Skip();
+    }
+
+    void Desistir(wxCommandEvent& evt) {
+        // É PRA FINALIZAR O JOGO E MOSTRAR OUTRA TELA
+        Destroy();
+
+    }
+    
+
+    // FUNCOES DO TIMER
+
+    void OnStartTimer(wxTimerEvent& evt)
+    {
+        m_startTime->Now() = wxDateTime::Now();
+        m_timer->Start(1000); // every second
+        UpdateDisplayedTime(); // update immediatelly do not wait a second
+    }
+
+    void OnStopTimer(wxTimerEvent& evt)
+    {
+        m_timer->Stop();
+        UpdateDisplayedTime();
+    }
+
+    void OnUpdateDisplayedTime(wxTimerEvent& evt)
+    {
+        UpdateDisplayedTime();
+    }
+
+    void UpdateDisplayedTime()
+    {
+        if (m_timer->IsRunning())
+        {
+            const wxTimeSpan ellapsedTime = wxDateTime::Now() - m_startTime->Now();
+
+            m_timeDisplay->SetLabel(ellapsedTime.Format("%D:%H:%M:%S"));
+        }
+        else
+        {
+            m_timeDisplay->SetLabel("  <Not Timing>  ");
+        }
+    }
+    
+    // CONSTRUTOR E DESTRUTOR
     cGame();
     ~cGame();
 
+    // BOTOES DO MENU
+    wxButton* btnDesistir = nullptr;
     wxButton* btnFinalizar = nullptr;
-    wxButton* btnFinalizar2 = nullptr;
 
-    wxVector<wxButton*> vecBtn;
 
+    // DE CLARAÇÃO DOS EVENTOS
     wxDECLARE_EVENT_TABLE();
 };
 
